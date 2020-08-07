c8a3b1771a6594e5046f12ea6e8aef8f05074b73
hongzh.zhang <hongzh.zhang@qunar.com>
2020-08-07 18:13:55 +0800

    update project

--- hive-learning/src/main/java/com/hong/hive/hello.java
+++ hive-learning/src/main/java/com/hong/hive/hello.java
@@ -1,7 +1,1 @@
-package com.hong.hive;
-
-/**
- * @author create by hongzh.zhang on 2020-08-07
- */
-public class hello {
-}
+package com.hong.hive;/** * @author create by hongzh.zhang on 2020-08-07 */public class hello {}
