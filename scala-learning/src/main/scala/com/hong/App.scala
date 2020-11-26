026a285d9493fd84890e73c2d39fefe7a66932a8
hongzh.zhang <hongzh.zhang@qunar.com>
2020-11-26 19:26:03 +0800

    scala-learning

---
+++ scala-learning/src/main/scala/com/hong/App.scala
@@ -0,0 +1,12 @@
+package com.hong
+
+/**
+  *
+  * 创建一个Scala的Object
+  * New -> Scala Class -> kind中选择Object
+  */
+object App {
+	def main(args: Array[String]): Unit = {
+		println( "Hello World!" )
+	}
+}
---
+++ scala-learning/src/main/scala/com/hong/Hello.scala
@@ -0,0 +1,9 @@
+package com.hong
+
+
+// New -> Scala Class -> kind中选择Object
+object Hello {
+  def main(args: Array[String]): Unit = {
+    println("hello")
+  }
+}
