11d8ba3a6c3eae7266e6cdffbc8436677a425ebe
hongzh.zhang <hongzh.zhang@qunar.com>
2020-12-17 19:48:03 +0800

    update

---
+++ base-learning/src/main/java/com/hong/base/OperatorStackTest.java
@@ -0,0 +1,16 @@
+package com.hong.base;
+
+/**
+ * @author create by hongzh.zhang on 2020-12-17
+ * 通过jclasslib查看字节码
+ */
+public class OperatorStackTest {
+    public void test1() {
+        String aa = "111";
+        {
+            String bb="22";
+        }
+
+        int num1 = 3;
+    }
+}
