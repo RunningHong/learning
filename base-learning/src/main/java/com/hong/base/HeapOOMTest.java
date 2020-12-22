d142cafc3a912a741da876519108c27184d92a61
hongzh.zhang <hongzh.zhang@qunar.com>
2020-12-22 20:10:03 +0800

    update

---
+++ base-learning/src/main/java/com/hong/base/HeapOOMTest.java
@@ -0,0 +1,23 @@
+package com.hong.base;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * @author create by hongzh.zhang on 2020-12-22
+ * 堆空间oom示例
+ * 运行前设置参数(Idea中Run->Edit Configurations谁知)： -Xms400m -Xmx400m
+ * cmd中通过jvisualvm调出堆可视化工具
+ */
+public class HeapOOMTest {
+    byte[] array = new byte[new Random().nextInt(1024*100)];
+
+    public static void main(String[] args) throws Exception {
+        ArrayList<HeapOOMTest> list = new ArrayList<>();
+        while (true) {
+            list.add(new HeapOOMTest());
+            Thread.sleep(30);
+        }
+    }
+
+}
