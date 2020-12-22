996265b96d03fa1753d8a3935556e4a2bb6b7f3d
hongzh.zhang <hongzh.zhang@qunar.com>
2020-12-22 20:18:46 +0800

    update

--- base-learning/src/main/java/com/hong/base/HeapOOMTest.java
+++ base-learning/src/main/java/com/hong/base/HeapOOMTest.java
@@ -6,7 +6,7 @@ import java.util.Random;
 /**
  * @author create by hongzh.zhang on 2020-12-22
  * 堆空间oom示例
- * 运行前设置参数(Idea中Run->Edit Configurations谁知)： -Xms400m -Xmx400m
+ * 运行前设置参数(Idea中Run->Edit Configurations设置)： -Xms400m -Xmx400m
  * cmd中通过jvisualvm调出堆可视化工具
  */
 public class HeapOOMTest {
