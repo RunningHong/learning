69222a561281afb66c9388457e7548ba031b6187
hongzh.zhang <hongzh.zhang@qunar.com>
2021-03-31 13:38:40 +0800

    stream

--- base-learning/src/main/java/com/hong/base/LambdaTest.java
+++ base-learning/src/main/java/com/hong/base/LambdaTest.java
@@ -2,10 +2,13 @@ package com.hong.base;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 /**
  * @author hongzh.zhang on 2021/03/28
@@ -114,10 +117,145 @@ public class LambdaTest {
     }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /**
+     * filter是断定型接口，接受一个输入，返回true/false，类似于sql中的where
+     */
     @Test
     public void testStreamFilter() {
-        System.out.println("d");
+        List<String> list = Arrays.asList("a", "bb", "bb", "cc", "ddd");
+        System.out.println("原列表：" + list.toString()); // 原列表：[a, bb, bb, cc, ddd]
+
+        // 返回新列表
+        list=list.stream()
+                .filter(str -> !str.equals("bb")) // 保留不为bb的数据
+                .collect(Collectors.toList());
+        System.out.println("新列表：" + list); // 新列表：[a, cc, ddd]
     }
+
+
+    /**
+     * distinct去重
+     */
+    @Test
+    public void testStreamDistinct() {
+        List<String> list = Arrays.asList("a", "bb", "bb", "cc", "ddd");
+        System.out.println("原列表：" + list.toString()); // 原列表：[a, bb, bb, cc, ddd]
+
+        // 返回新列表
+        list=list.stream()
+                .distinct() // 按遍历值去重
+                .collect(Collectors.toList());
+        System.out.println("新列表：" + list); // 新列表：[a, bb, cc, ddd]
+    }
+
+    /**
+     * limit限制条数
+     */
+    @Test
+    public void testStreamLimit() {
+        List<String> list = Arrays.asList("a", "bb", "bb", "cc", "ddd");
+        System.out.println("原列表：" + list.toString()); // 原列表：[a, bb, bb, cc, ddd]
+
+        // 返回新列表
+        list=list.stream()
+                .limit(2) // 只返回前两条数据
+                .collect(Collectors.toList());
+        System.out.println("新列表：" + list); // 新列表：[a, bb]
+    }
+
+
+    /**
+     * sorted-排序
+     * 被排序的数据需要可比较即Comparable
+     * 可以自定义比较器
+     */
+    @Test
+    public void testStreamSorted() {
+        List<String> list = Arrays.asList("a", "bb", "bb", "cc", "ddd");
+        System.out.println("原列表：" + list.toString()); // 原列表：[a, bb, bb, cc, ddd]
+
+        // 返回新列表
+        list=list.stream()
+                .sorted() // 按照默认比较器比较
+                .collect(Collectors.toList());
+        System.out.println("新列表：" + list); // 新列表：[a, bb, bb, cc, ddd]
+
+        list=list.stream()
+                .sorted( // 自定义比较器
+                    (s1, s2)->s2.length()-s1.length()
+                )
+                .collect(Collectors.toList());
+        System.out.println("新列表：" + list); // 新列表：[ddd, bb, bb, cc, a]
+    }
+
+
+    /**
+     * skip -  跳过n个元素后再输出
+     */
+    @Test
+    public void testStreamSkip() {
+        List<String> list = Arrays.asList("a", "bb", "bb", "cc", "ddd");
+        System.out.println("原列表：" + list.toString()); // 原列表：[a, bb, bb, cc, ddd]
+
+        // 返回新列表
+        list=list.stream()
+                .skip(2) // 跳过前两个元素
+                .collect(Collectors.toList());
+        System.out.println("新列表：" + list); // 新列表：[bb, cc, ddd]
+    }
+
+    /**
+     * map - 对每个元素做特定的操作
+     * map是函数型接口，允许一个输入一个输出
+     */
+    @Test
+    public void testStreamMap() {
+        List<String> list = Arrays.asList("a", "bb", "bb", "cc", "ddd");
+        System.out.println("原列表：" + list.toString()); // 原列表：[a, bb, bb, cc, ddd]
+
+        // 返回新列表
+        list=list.stream()
+                .map(str-> str.toUpperCase()) // 对遍历的结果做特定操作-转换为大写
+                .collect(Collectors.toList());
+        System.out.println("新列表：" + list); // 新列表：[A, BB, BB, CC, DDD]
+    }
+
+
+    /**
+     * flatMap -  将遍历的值转成一个新的流
+     */
+    @Test
+    public void testStreamFlatMap() {
+        String[] strArray = {"Hello", "World"};
+
+        List list1 = Arrays.asList(strArray).stream()
+                .map(str -> str.split("")) // 将字符串转换为字符串数组
+                .collect(Collectors.toList());
+        System.out.println(list1.toString()); // [[Ljava.lang.String;@5c8da962, [Ljava.lang.String;@512ddf17]
+
+        List list2 = Arrays.asList(strArray).stream()
+                .map(str -> str.split("")) // 将字符串转换为字符串数组
+                .flatMap(Arrays::stream) // 将前面的结果转换为新的流
+                .collect(Collectors.toList());
+        System.out.println(list2.toString()); // [H, e, l, l, o, W, o, r, l, d]
+    }
+
 
 
 }
